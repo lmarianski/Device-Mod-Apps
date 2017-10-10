b3001a646a5499f106bdcf4310c6f1ccd10c215d
lukas2005 <lukas2005.38@gmail.com>
2017-10-10 16:57:44 +0200

    starting to imolement baisic functioinality

--- src/main/java/io/github/lukas2005/WebBrowserApp/apps/ApplicationWebBrowser.java
+++ src/main/java/io/github/lukas2005/WebBrowserApp/apps/ApplicationWebBrowser.java
@@ -1,6 +1,7 @@
 package io.github.lukas2005.WebBrowserApp.apps;
 
 import com.mrcrayfish.device.api.app.Application;
+import com.mrcrayfish.device.api.app.Layout;
 
 import io.github.lukas2005.WebBrowserApp.Reference;
 import net.minecraft.nbt.NBTTagCompound;
@@ -11,6 +12,14 @@ public class ApplicationWebBrowser extends Application {
 		super(Reference.MOD_ID, "Mineium Web Browser");
 	}
 
+	@Override
+	public void init() {
+		Layout main = new Layout();
+		this.setCurrentLayout(main);
+		
+		
+	}
+	
 	@Override
 	public void load(NBTTagCompound tagCompound) {
 		// TODO Auto-generated method stub
---
+++ src/main/java/io/github/lukas2005/WebBrowserApp/components/WebBrowserComponent.java
@@ -0,0 +1,21 @@
+package io.github.lukas2005.WebBrowserApp.components;
+
+import com.mrcrayfish.device.api.app.Component;
+import com.mrcrayfish.device.core.Laptop;
+
+import net.minecraft.client.Minecraft;
+
+public class WebBrowserComponent extends Component {
+
+	public WebBrowserComponent(int left, int top) {
+		super(left, top);
+	}
+
+	@Override
+	public void render(Laptop laptop, Minecraft mc, int x, int y, int mouseX, int mouseY, boolean windowActive,
+			float partialTicks) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
