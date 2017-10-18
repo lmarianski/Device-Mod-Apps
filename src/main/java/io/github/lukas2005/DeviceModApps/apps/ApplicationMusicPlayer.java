27e4ca2351fbc3a5f853347763b1067999ec9cc9
lukas2005 <lukas2005.38@gmail.com>
2017-10-18 21:42:08 +0200

    will be using sound cloud when you will be able ot register aoos again

--- src/main/java/io/github/lukas2005/DeviceModApps/apps/ApplicationMusicPlayer.java
+++ src/main/java/io/github/lukas2005/DeviceModApps/apps/ApplicationMusicPlayer.java
@@ -1,11 +1,14 @@
 package io.github.lukas2005.DeviceModApps.apps;
 
 import com.mrcrayfish.device.api.app.Layout;
+import com.soundcloud.api.ApiWrapper;
 
 import net.minecraft.nbt.NBTTagCompound;
 
 public class ApplicationMusicPlayer extends ApplicationBase {
-
+	
+	ApiWrapper api = new ApiWrapper("", "", null, null);
+	
 	@Override
 	public void init() {
 		Layout main = new Layout();
