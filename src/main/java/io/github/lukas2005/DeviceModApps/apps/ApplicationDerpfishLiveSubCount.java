089148930677b2ba968f28282156e7529f29228c
lukas2005 <lukas2005.38@gmail.com>
2017-10-27 14:52:58 +0200

    removed the debug sysout

--- src/main/java/io/github/lukas2005/DeviceModApps/apps/ApplicationDerpfishLiveSubCount.java
+++ src/main/java/io/github/lukas2005/DeviceModApps/apps/ApplicationDerpfishLiveSubCount.java
@@ -21,55 +21,48 @@ public class ApplicationDerpfishLiveSubCount extends ApplicationBase {
 	
 	@Override
 	public void init() {
-		try {
-			Layout main = new Layout(200, 130);
-			setCurrentLayout(main);
-			
-			Image derpfishFace = new Image(10, 10, 50, 50, "http://yt3.ggpht.com/-3GSWfHL9moQ/AAAAAAAAAAI/AAAAAAAAAAA/0tcO6ddi8eY/s88-c-k-no-mo-rj-c0xffffff/photo.jpg");
-			Text derpfishName = new Text("MrCrayfish", 65, 20, main.width);
-			final Text derpfishSubCount = new Text("0", 65, 30, main.width);
-			
-			main.addComponent(derpfishName);
-			main.addComponent(derpfishSubCount);
-			main.addComponent(derpfishFace);
-			
-			System.out.println(YoutubeUtils.getChannelIcon(MRCRAYFISH_CHANNEL_ID));
-			
-			Image derpfishDevFace = new Image(10, 70, 50, 50, "http://yt3.ggpht.com/-AzEP6eQw3Nc/AAAAAAAAAAI/AAAAAAAAAAA/BKsMPLothZE/s88-c-k-no-mo-rj-c0xffffff/photo.jpg");
-			Text derpfishDevName = new Text("MrCrayfishDev", 65, 80, main.width);
-			final Text derpfishDevSubCount = new Text("0", 65, 90, main.width);
-			
-			main.addComponent(derpfishDevName);
-			main.addComponent(derpfishDevSubCount);
-			main.addComponent(derpfishDevFace);
-			
-			Text aboutText = new Text("Requested by vos6434", main.width-115, main.height-10, main.width);
-			//Text aboutText2 = new Text("Made by lukas2005", main.width-95, main.height-10, main.width);
-			
-			main.addComponent(aboutText);
-			//main.addComponent(aboutText2);
-			
-			subUpdateThread = new Thread("Sub Update Thread") {
-				public void run() {
-					while (!Thread.interrupted()) {
-						try {
-							derpfishSubCount.setText(YoutubeUtils.getSubscriberCount(MRCRAYFISH_CHANNEL_ID)+"");
-							derpfishDevSubCount.setText(YoutubeUtils.getSubscriberCount(MRCRAYFISHDEV_CHANNEL_ID)+"");
-							Thread.sleep(5000);
-						} catch (IOException e) {
-							e.printStackTrace();
-						} catch (InterruptedException e) {
-							Thread.currentThread().interrupt();
-						}
+		Layout main = new Layout(200, 130);
+		setCurrentLayout(main);
+		
+		Image derpfishFace = new Image(10, 10, 50, 50, "http://yt3.ggpht.com/-3GSWfHL9moQ/AAAAAAAAAAI/AAAAAAAAAAA/0tcO6ddi8eY/s88-c-k-no-mo-rj-c0xffffff/photo.jpg");
+		Text derpfishName = new Text("MrCrayfish", 65, 20, main.width);
+		final Text derpfishSubCount = new Text("0", 65, 30, main.width);
+		
+		main.addComponent(derpfishName);
+		main.addComponent(derpfishSubCount);
+		main.addComponent(derpfishFace);
+		
+		Image derpfishDevFace = new Image(10, 70, 50, 50, "http://yt3.ggpht.com/-AzEP6eQw3Nc/AAAAAAAAAAI/AAAAAAAAAAA/BKsMPLothZE/s88-c-k-no-mo-rj-c0xffffff/photo.jpg");
+		Text derpfishDevName = new Text("MrCrayfishDev", 65, 80, main.width);
+		final Text derpfishDevSubCount = new Text("0", 65, 90, main.width);
+		
+		main.addComponent(derpfishDevName);
+		main.addComponent(derpfishDevSubCount);
+		main.addComponent(derpfishDevFace);
+		
+		Text aboutText = new Text("Requested by vos6434", main.width-115, main.height-10, main.width);
+		//Text aboutText2 = new Text("Made by lukas2005", main.width-95, main.height-10, main.width);
+		
+		main.addComponent(aboutText);
+		//main.addComponent(aboutText2);
+		
+		subUpdateThread = new Thread("Sub Update Thread") {
+			public void run() {
+				while (!Thread.interrupted()) {
+					try {
+						derpfishSubCount.setText(YoutubeUtils.getSubscriberCount(MRCRAYFISH_CHANNEL_ID)+"");
+						derpfishDevSubCount.setText(YoutubeUtils.getSubscriberCount(MRCRAYFISHDEV_CHANNEL_ID)+"");
+						Thread.sleep(5000);
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
 					}
-				};
+				}
 			};
-			
-			subUpdateThread.start();
-			
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+		};
+		
+		subUpdateThread.start();
 	}
 	
 	@Override
